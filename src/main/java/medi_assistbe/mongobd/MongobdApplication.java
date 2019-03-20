ef21168f17e129052cb2cec94c63758bec103ee7
DanishAli01 <danishali2158@gmail.com>
2019-03-20 01:48:28 +0000

    init

---
+++ src/main/java/medi_assistbe/mongobd/MongobdApplication.java
@@ -0,0 +1,13 @@
+package medi_assistbe.mongobd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongobdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongobdApplication.class, args);
+    }
+
+}
