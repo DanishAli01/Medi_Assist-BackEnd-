1718dc9bcd9a37730e67a91bda64595b04e580e7
DanishAli01 <danishali2158@gmail.com>
2019-03-20 21:36:07 +0000

    password in header (springboottutorial.com/securing-rest-services-with-spring-boot-starter-security) removed Client Apitoken and new classes added (youtube.com/watch?v=wZeYU85s_oA)
    Signed-off-by: DanishAli01 <danishali2158@gmail.com>

--- src/main/java/medi_assistbe/mongobd/MongoDBRestController.java
+++ src/main/java/medi_assistbe/mongobd/MongoDBRestController.java
@@ -1,18 +1,11 @@
 package medi_assistbe.mongobd;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
